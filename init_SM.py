ce4b2b12d25fd0c87a70d555c73f3276f496cf66
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-03-21 17:57:17 +0000

    Copies the UserConfig file in on SM download

---
+++ init_SM.py
@@ -0,0 +1,21 @@
+# init_SM.py
+# initialisation scipt run on download of this Service Module
+
+# Get location of this script
+
+# Take init action:
+
+# The following should probably be done within docker/volumes/environment variables,
+#   but not being a docker expert the way I can get working now is using python.
+#
+# The purpose is to make the UserConfig/sensor_config.toml file contents 
+#   available to the ServiceModules/Sensing/temperature_dc docker container.
+# Copy Solution's user config file to within the container:
+
+from pathlib import Path
+import shutil
+shutil.copyfile(str(Path(__file__).parents[3].joinpath("UserConfig/sensor_config.toml")), str(Path(__file__).parents[0].joinpath("temperature_dc/config/sensor_config.toml")))
+
+# A forseeable problem with this approach is that the config file must be edited before this script is run,
+# If the userconfig is edited after the service modules have been downloaded, it won't be loaded. 
+# This must be improved!
