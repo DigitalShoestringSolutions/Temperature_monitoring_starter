64787a8e2fe740c277ad12a22844897649bf32a9
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-03-25 12:36:07 +0000

    Feature/type naming (#8)
    Updated a lot of licensing headers - many talked about Power Monitoring.
    Removed the redundant [type] config option. The type of sensing is contained entirely within the sensor selected.
    Renamed some sensing elements for clarity & better logging messages
    Restored the sensing stack for a couple of sensors (sht30, max6675, max31855) which had links missing.
    Removed some disused files (calculate.py)

--- temperature_dc/code/adc/MAX31865.py
+++ temperature_dc/code/adc/MAX31865.py
@@ -1,6 +1,30 @@
-# MAX31865.py
-# Toby Harris 2024 @ IfM Engage
-# Usage: see test section at end of file
+# ----------------------------------------------------------------------
+#
+#    Temperature Monitoring (Basic solution) -- This digital solution enables, measures,
+#    reports and records different  types of temperatures (contact, air, radiated)
+#    so that the temperature conditions surrounding a process can be understood and 
+#    taken action upon. Suppored sensors include 
+#    k-type thermocouples, RTDs, air samplers, and NIR-based sensors.
+#    The solution provides a Grafana dashboard that 
+#    displays the temperature timeseries, set threshold value, and a state timeline showing 
+#    the chnage in temperature. An InfluxDB database is used to store timestamp, temperature, 
+#    threshold and status. 
+#
+#    Copyright (C) 2022  Shoestring and University of Cambridge
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see https://www.gnu.org/licenses/.
+#
+# ----------------------------------------------------------------------
 
 import spidev
 from math import sqrt
