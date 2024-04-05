2b894b880336886de28926b0a75998acb9edc52c
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-04-05 16:49:03 +0100

    Update init_SM.py
    init_SM.py creates a hard link rather than copying config file

--- init_SM.py
+++ init_SM.py
@@ -10,19 +10,17 @@
 #
 # The purpose is to make the UserConfig/sensor_config.toml file contents 
 #   available to the ServiceModules/Sensing/temperature_dc docker container.
-# Copy Solution's user config file to within the container:
+
+
+# Create hard nonsymbolic Unix link between the file in UserConfig and the SM:
 
 from pathlib import Path
-import shutil
+import os
 
 # Note that due to the dubious exec(f.read()) that actually runs this script, 
 #   Path(__file__) returns the Assembly/ShoestringAssembler location!
 
-copy_from = str(Path(__file__).parents[3].joinpath("UserConfig/sensor_config.toml"))
-copy_to = str(Path(__file__).parents[2].joinpath("Sensing/temperature_dc/config/sensor_config.toml"))
+link_from_abs = Path(__file__).parents[3].joinpath("UserConfig/sensor_config.toml")
+link_to_abs = Path(__file__).parents[2].joinpath("Sensing/temperature_dc/config/sensor_config.toml")
 
-shutil.copyfile(copy_from, copy_to)
-
-# A forseeable problem with this approach is that the config file must be edited before this script is run,
-# If the userconfig is edited after the service modules have been downloaded, it won't be loaded. 
-# This must be improved!
+os.system("ln " + str(link_from_abs) + " " + str(link_to_abs))
