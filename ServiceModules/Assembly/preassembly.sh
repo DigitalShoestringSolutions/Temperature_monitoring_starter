96a46fa1b2d5389ddc2b8ea6e42ea5b926c1e3cf
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-06-18 11:07:57 +0100

    Version lock on Assembler in preassembly.sh
    Since the assembler has been developed further and it's public API has changed. Updating this example to use a future version of Assembler (recommended) needs testing on hardware.

--- ServiceModules/Assembly/preassembly.sh
+++ ServiceModules/Assembly/preassembly.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Download Shoestring Assembler
 echo "Downloading Shoestring Assembler..."
-git clone https://github.com/DigitalShoestringSolutions/ShoestringAssembler -b stable $SCRIPT_DIR/ShoestringAssembler
+git clone https://github.com/DigitalShoestringSolutions/ShoestringAssembler -b v0.1.0 $SCRIPT_DIR/ShoestringAssembler
 
 # Run Shotestring Assembler
 echo "Running Shoestring Assembler..."
